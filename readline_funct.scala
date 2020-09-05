c37cc5105f6fbb7c299260fc3a912f195cb95253
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-09-05 22:13:34 +0530

    Add files via upload

---
+++ readline_funct.scala
@@ -0,0 +1,18 @@
+
+
+object readline_funct {
+  
+ def main(args: Array[String])
+ {
+while (true) {
+
+val result=scala.io.StdIn.readLine()//function to read from console
+
+printf("Value inputed is %s", result)
+
+println()
+}
+
+ }
+}
+  
