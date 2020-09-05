ceff57af33f5eac12be789d94643037470af286e
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-09-05 22:13:48 +0530

    Add files via upload

---
+++ printcommand.scala
@@ -0,0 +1,15 @@
+
+object printcommand {
+  // Main method
+def main(args: Array[String])
+{ 
+Console.println("I am a Btech Student")//prints and shifts control to next line
+
+print("Fond of coding")//prints and control stays in same line
+print(" and machines")
+
+println()//next line
+
+printf("my age is: %d", 19)
+}
+}
